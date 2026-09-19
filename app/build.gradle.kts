649fe968fbbb4b02c14f2c8be019a3e4d84d4afc
allabiely-bit <allabiely@gmail.com>
2026-09-19 23:07:55 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,15 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-android {
+android { 
+compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
     namespace = "com.harmonyvoice.app"
     compileSdk = 35
 
