53775a8f9c4947359e5ac6f884799e9a9eaf7c7d
allabiely-bit <allabiely@gmail.com>
2026-09-22 16:09:03 +0100

    Add audio processing engine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,4 +28,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+
+    // Moteur de traitement audio pour les harmonies
+    implementation("com.tianscar.soundtouch:soundtouch-jni-android:1.1.1")
 }
